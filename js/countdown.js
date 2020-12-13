8fd29cfa6bc5369bb23719acf5ff66db25ac09bd
HeartCaptureCB <heartcapturecb@gmail.com>
2020-12-13 21:56:15 +0800

    Change countdown method

--- js/countdown.js
+++ js/countdown.js
@@ -1,31 +1,72 @@
-// Set the date we're counting down to
-var date_string = "January 5, 2021 00:00:00 GMT"
-var countDownDate = new Date(date_string).getTime();
+(function () {
+  const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
 
-// Update the count down every 1 second
-var x = setInterval(function() {
+  // let birthday = "Jan 5, 2021 00:00:00 GMT",
+  let birthday = "Dec 13, 2020 14:13:00 GMT",
+      countDown = new Date(birthday).getTime(),
+      x = setInterval(function() {    
 
-  // Get today's date and time
-  var now = new Date().getTime();
+        let now = new Date().getTime(),
+            distance = countDown - now;
 
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
+          document.getElementById("days").innerText = Math.floor(distance / (day)),
+          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
 
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        //do something later when date is reached
+        if (distance < 0) {
+          let headline = document.getElementById("headline"),
+              countdown = document.getElementById("countdown"),
+              content = document.getElementById("content");
 
-  // Display the result in the element with id="demo"
-  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+            document.getElementById("days").innerText = 0,
+            document.getElementById("hours").innerText = 0,
+            document.getElementById("minutes").innerText = 0,
+            document.getElementById("seconds").innerText = 0;
+    
+          headline.innerText = "END";
+          countdown.style.display = "none";
+          content.style.display = "block";
 
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("countdown").innerHTML = "ENDED";
-  }
+          clearInterval(x);
+        }
+        //seconds
+        document.getElementById("deadline").innerHTML = birthday;
+      }, 0);
+  }());
 
-  document.getElementById("deadline").innerHTML = date_string;
-}, 1000);
+// // Set the date we're counting down to
+// var date_string = "January 5, 2021 00:00:00 GMT"
+// var countDownDate = new Date(date_string).getTime();
+
+// // Update the count down every 1 second
+// var x = setInterval(function() {
+
+//   // Get today's date and time
+//   var now = new Date().getTime();
+
+//   // Find the distance between now and the count down date
+//   var distance = countDownDate - now;
+
+//   // Time calculations for days, hours, minutes and seconds
+//   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+//   // Display the result in the element with id="demo"
+//   document.getElementById("countdown").innerHTML = days + " " + hours + "h "
+//   + minutes + "m " + seconds + "s ";
+
+//   // If the count down is finished, write some text
+//   if (distance < 0) {
+//     clearInterval(x);
+//     document.getElementById("countdown").innerHTML = "ENDED";
+//   }
+
+//   document.getElementById("deadline").innerHTML = date_string;
+// }, 1000);
