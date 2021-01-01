a1365283f1f0d2c863793a5f33286442b76f998b
HeartCaptureCB <heartcapturecb@gmail.com>
2021-01-01 19:53:28 +0800

    add back to top function

--- js/template-scripts.js
+++ js/template-scripts.js
@@ -78,3 +78,29 @@ function divToggle(elem) {
 //     }
 //   });
 // });
+
+//Get the button:
+mybutton = document.getElementById("scroll-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     mybutton.style.display = "block";
+//   } else {
+//     mybutton.style.display = "none";
+//   }
+// }
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  var position =
+    document.body.scrollTop || document.documentElement.scrollTop;
+  if (position) {
+    window.scrollBy(0, -Math.max(1, Math.floor(position / 8)));
+    scrollAnimation = setTimeout("topFunction()", 15);
+  } else clearTimeout(scrollAnimation);
+  // document.body.scrollTop = 0; 
+  // document.documentElement.scrollTop = 0; 
+}
